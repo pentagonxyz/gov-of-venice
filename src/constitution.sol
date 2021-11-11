3a67459a1b03e00fd16b54820c9b09effd8b59ce
Odysseas Lamtzidis <odyslam@gmail.com>
2021-11-11 11:08:20 +0300

    add constitution function skeleton

---
+++ src/constitution.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.9;
+
+contract Constitution {
+
+    function initialise();
+
+    function setMerchantRepublic();
+
+    function queue();
+
+    function _queueOrRevert();
+
+    function execute();
+
+}
