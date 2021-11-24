17bba6565055c5bcf215ce18b2597379ff6dc9c8
Odysseas Lamtzidis <odyslam@gmail.com>
2021-11-24 16:43:19 +0300

    feat: TokensI interface

---
+++ src/tokensI.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.9;
+
+interface TokensI{
+    function balanceOf(address account) external view returns (uint256);
+    function transfer(address recipient, uint256 amount) external returns (bool);
+}
+
