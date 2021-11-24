dd6dbc400f4f77683be39b26d7ee4bf3efb27dec
Odysseas Lamtzidis <odyslam@gmail.com>
2021-11-24 16:32:59 +0300

    feat: guildI interface

---
+++ src/guildI.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.9;
+
+
+interface GuildI {
+    function calculateGravitas(address commonerAddress, uint256 silverAmount)
+        public
+        returns (uint256 gravitas);
+    function modifyGravitas(address guildMember, uint256 newGravitas)
+        external
+        onlyGuildCouncil
+        returns (uint256 newGuildMemberGravitas);
+    function appendChainOfResponsbility(address guildMember, address commoner)
+        external
+        onlyGuildCouncil
+        returns (bool success);
+    function guildVoteRequest(uint256 proposalId)
+        external
+        onlyGuildCouncil;
+}
