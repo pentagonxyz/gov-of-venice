6ddc8868dbef6b14b6182a495bf1fa424322f59f
Odysseas Lamtzidis <odyslam@gmail.com>
2021-11-30 21:42:47 +0200

    feat: gov2Test base contract for all tests

--- src/test/utils/gov2Test.sol
+++ src/test/utils/gov2Test.sol
@@ -1,3 +1,6 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.9;
+
 import {DSTestPlus} from "solmate/test/utils/DSTestPlus.sol";
 import {MockERC20} from "solmate/test/utils/mocks/MockERC20.sol";
 import "./Hevm.sol";
@@ -6,47 +9,84 @@ import {GuildCouncil} from "../../guildCouncil.sol";
 import {MerchantRepublic} from "../../merchantRepublic.sol";
 import {Constitution} from "../../constitution.sol";
 
-contract mockConstitution is Constitution {
+contract MockConstitution is Constitution {
 
     GuildCouncil guildCouncil;
-    MerchantRepublic merchantRepublic;
+    MerchantRepublic merchantRepublicContract;
 
 
-    function mockProposals(address gc, address gc){
+    function mockProposals(address gc, address mr) public {
         guildCouncil = GuildCouncil(gc);
-        merchantRepublic = MerchantRepublic(mr);
+        merchantRepublicContract = MerchantRepublic(mr);
     }
 
-    function mockEstablishGuild(address guild){
-        guildCouncil.establishGuild(guild);
+    function mockEstablishGuild(address guild) public returns(uint256){
+        return guildCouncil.establishGuild(guild);
     }
 
 }
 
-contract Commoner{
+contract Commoner is DSTestPlus{
     Guild internal g;
     GuildCouncil internal gc;
     MerchantRepublic internal mr;
-    Constitution internal con;
+    MockConstitution internal con;
     MockERC20 internal md;
 
     constructor(){}
 
-    function init(address _g, address _gc, address _mr, address _con, address _md){
-        g = Guild(_g);
-        gc = Guildcouncil(_gc);
+    function init( address _gc, address _mr, address _con, address _md)
+        public
+    {
+        gc = GuildCouncil(_gc);
         mr = MerchantRepublic(_mr);
-        con = Constitution(_con);
+        con = MockConstitution(_con);
         md = MockERC20(_md);
     }
+
+    function setGuildAddress(address _g)
+        public
+    {
+        g = Guild(_g);
+    }
+
+    function sendSilver(address rec, uint256 amount, uint256 guildId)
+        public
+        returns(uint256)
+    {
+        return mr.sendSilver(rec, amount, guildId);
+    }
+
+    function setSilverSeason()
+        public
+        returns(bool)
+    {
+        return mr.setSilverSeason();
+    }
+
+    function silverBalance()
+        public
+        returns(uint256)
+    {
+        return mr.silverBalance();
+    }
+
+    function initializeMerchantRepublic(address conAddr, address tokAddr, address gcAddr,
+                                       uint votingPeriod, uint votingDelay, uint propThres)
+        public
+    {
+        mr.initialize(conAddr, tokAddr, gcAddr, votingPeriod, votingDelay, propThres);
+        mr._initiate(address(0));
+    }
 }
+
 contract Gov2Test is DSTestPlus {
 
     Guild internal guild;
     GuildCouncil internal guildCouncil;
     MerchantRepublic internal merchantRepublic;
-    Constitution internal constitution;
-    MockERC20 internal mockERC20;
+    MockConstitution internal constitution;
+    MockERC20 internal mockDucat;
 
     Commoner internal ursus;
     Commoner internal agnello;
@@ -55,7 +95,12 @@ contract Gov2Test is DSTestPlus {
 
     Guild internal locksmiths;
     Guild internal blacksmiths;
-    Guidl internal judges;
+    Guild internal judges;
+
+    uint256 agnelloDucats;
+    uint256 johnDucats;
+    uint256 ursusDucats;
+    uint256 pipinDucats;
 
     function setUp() public virtual {
         ursus = new Commoner();
@@ -64,22 +109,56 @@ contract Gov2Test is DSTestPlus {
         pipin = new Commoner();
 
         // Create the ERC20 gov token
-        mockDucat= new MockERC20("Ducat Token" "DK", 18);
+        mockDucat= new MockERC20("Ducat Token", "DK", 18);
         // Create the gov modules
-        merchantRepublic = new MerchantRepublic();
-        constitution = new mockConstitution();
-        guildCouncil = new GuildCouncil(address(merchantRepublic), address(guildCouncil), address(mockDucat));
+        merchantRepublic = new MerchantRepublic(address(ursus));
+        constitution = new MockConstitution();
+        guildCouncil = new GuildCouncil(address(merchantRepublic), address(constitution), address(mockDucat));
+
+        ursus.init(address(guildCouncil), address(merchantRepublic), address(constitution), address(mockDucat));
+        agnello.init(address(guildCouncil), address(merchantRepublic), address(constitution), address(mockDucat));
+        john.init(address(guildCouncil), address(merchantRepublic), address(constitution), address(mockDucat));
+        pipin.init(address(guildCouncil), address(merchantRepublic), address(constitution), address(mockDucat));
+
         constitution.signTheConstitution(address(merchantRepublic), 2 days);
         constitution.mockProposals(address(guildCouncil), address(merchantRepublic));
+        ursus.initializeMerchantRepublic(address(constitution), address(mockDucat), address(guildCouncil), 14 days, 2 days, 10);
 
-        locksmiths = new Guild("locksmiths", 100, 14 days, 15, 7 days);
-        blacksmiths = new Guild("blacksmiths", 50, 7 days, 50, 4 days);
-        judges = new Guild("judges", 400, 25 days, 5, 14 days);
+        address[] memory founding1 = new address[](1);
+        founding1[0] = address(ursus);
+        address[] memory founding2 =  new address[](2);
+        founding2[0] = address(agnello);
+        founding2[1] = address(ursus);
+        address[] memory founding3 = new address[](1);
+        founding3[0] = address(john);
+
+
+        locksmiths = new Guild("locksmiths", founding1, 100, 14 days, 15, 7 days, address(mockDucat), address(constitution));
+        blacksmiths = new Guild("blacksmiths", founding2, 50, 7 days, 50, 4 days, address(mockDucat), address(constitution));
+        judges = new Guild("judges", founding3, 400, 25 days, 5, 14 days, address(mockDucat), address(constitution));
 
         // Register the guilds with the GuildCouncil
-        constitution.mockEstablishGuild(address(locksmiths));
-        constitution.mockEstablishGuild(address(blacksmiths));
-        constitution.mockEstablishGuild(address(judges));
+        uint256 locksmithsId = constitution.mockEstablishGuild(address(locksmiths));
+        uint256 blacksmithsId = constitution.mockEstablishGuild(address(blacksmiths));
+        uint256 judgesId = constitution.mockEstablishGuild(address(judges));
+        assertEq(locksmithsId, 0);
+        assertEq(blacksmithsId, 1);
+        assertEq(judgesId, 2);
+
+        // mint $ducats
+        ursusDucats = 10000;
+        agnelloDucats = 20000;
+        johnDucats = 1000;
+        pipinDucats = 500;
+        mockDucat.mint(address(ursus), ursusDucats);
+        mockDucat.mint(address(agnello), agnelloDucats);
+        mockDucat.mint(address(john), johnDucats);
+        mockDucat.mint(address(pipin), pipinDucats);
+
+        // Ursus is the Doge and sets the silver season
+        ursus.setSilverSeason();
+
+
 
     }
 }
