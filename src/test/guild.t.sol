ef061f67d74eb3b425542b0c1db4c9ccb142c41d
Odysseas Lamtzidis <odyslam@gmail.com>
2021-12-01 23:05:38 +0200

    feat: test gravitas calculation

--- src/test/guild.t.sol
+++ src/test/guild.t.sol
@@ -5,6 +5,18 @@ import "./utils/gov2Test.sol";
 
 contract GuildTest is Gov2Test {
 
+//
+//
+ function testGravitasCalculation() public {
+        uint256 remain1 = agnello.sendSilver(address(john), 300, 2);
+        uint256 remain2 = john.sendSilver(address(agnello), 500, 2);
+        // new_gravitas = 10% of silver_sent + 50% of sender gravitas + prior gravitas
+        assertEq(30 + 0 + 500, john.getGravitas(2));
+        // john has now 530 gravitas, as he got 30 from the silver
+        // from agnello
+        assertEq(50 + 265 + 0, agnello.getGravitas(2));
+ }
+
 
 
 }
